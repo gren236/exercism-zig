d1bee44980407eba0313d24e495e6feeddf45298
Egor Rudkov <gren236@gmail.com>
2025-06-26 21:53:57 +0200

    add resistor color duo solution

---
+++ resistor-color-duo/resistor_color_duo.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+pub const ColorBand = enum {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
+pub fn colorCode(colors: [2]ColorBand) usize {
+    return @as(u16, @intFromEnum(colors[0])) * 10 + @intFromEnum(colors[1]);
+}
